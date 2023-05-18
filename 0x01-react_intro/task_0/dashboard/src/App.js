91feb032744a3d7b8ae5d64032c33bdd2e4607cd
emachizy <emachi2011@gmail.com>
2023-05-17 19:34:23 -0700

    react

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src="holberton-logo.png" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
